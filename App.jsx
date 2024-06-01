481147796266744a6975c740e2799056bf44146d
dhanush11042004 <m.dhanush6757334@gmail.com>
2024-06-01 21:46:20 +0530

    final

--- App.jsx
+++ App.jsx
@@ -1,38 +1,38 @@
-
+// App.jsx
 import './index.css'
-import React from 'react'
-import logo from './assets/logo.png'
-import searchicon from './assets/searchicon.png'
+import React, { createContext, useContext, useState } from 'react'
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import EntertainmentNews from './components/Entertainment'
+import Generalnews from "./components/General"
+import SportsNews from './components/Sprt'
+import TechnologyNews from './components/Technology'
+import Search from './components/Search' // Import the Search component
 import Switching from './components/Switching'
-import {BrowserRouter,Route,Routes} from "react-router-dom"
-import Sprt from './components/Sprt'
-import Entertainment from './components/Entertainment'
-import General from "./components/General"
+
+export const countries = createContext()
 
 function App() {
+  const [selectedCountry, setselectedCountry] = useState("india")
   return (
     <>
-    
       <div className='dash'>
-      <BrowserRouter>
-      <Switching/>
-       <div className='containers'>
-        <Routes>
-          <Route path="/sports" element={<Sprt/>}></Route>
-        </Routes>
-        <Routes>
-          <Route path="/general" element={<General/>}></Route>
-        </Routes>
-        <Routes>
-          <Route path="/Entertainment" element={<Entertainment/>}></Route>
-        </Routes>
-       </div>
-      </BrowserRouter>
-      </div> 
-      
-      
-      
-    
+        <BrowserRouter>
+          <countries.Provider value={{ selectedCountry, setselectedCountry }}>
+            <Switching />
+          </countries.Provider>
+          <div className='containers'>
+            <Routes>
+            <Route path="/" element={<countries.Provider value={{ selectedCountry, setselectedCountry }}><Search /></countries.Provider>} />
+            <Route path="/search" element={<countries.Provider value={{ selectedCountry, setselectedCountry }}><Search /></countries.Provider>} />
+              <Route path="/sports" element={<countries.Provider value={{ selectedCountry, setselectedCountry }}><SportsNews /></countries.Provider>} />
+              <Route path="/general" element={<countries.Provider value={{ selectedCountry, setselectedCountry }}><Generalnews /></countries.Provider>} />
+              <Route path="/entertainment" element={<countries.Provider value={{ selectedCountry, setselectedCountry }}><EntertainmentNews /></countries.Provider>} />
+              <Route path="/technology" element={<countries.Provider value={{ selectedCountry, setselectedCountry }}><TechnologyNews /></countries.Provider>} />
+             
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </div>
     </>
   )
 }
